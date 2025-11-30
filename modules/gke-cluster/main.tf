3cb2c2f52630cb1199c1b25a63f4a223653b9577
Aivaras Sukackas <aivarassukackas@MacBookPro.lan>
2025-11-30 22:44:40 +0200

    update

--- modules/gke-cluster/main.tf
+++ modules/gke-cluster/main.tf
@@ -384,7 +384,7 @@ resource "google_container_node_pool" "node_pool_additional" {
 module "gke_service_account" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
-  source                = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-service-account?ref=v0.3.8"
+  source                = "./gke-sa"
   name                  = "${var.cluster_service_account_name}${var.sufix}"
   project               = var.project
   description           = var.cluster_service_account_description
