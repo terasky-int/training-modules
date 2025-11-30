181ddce839a6d177c3f8a66b8b62202f9ef828e3
Aivaras Sukackas <aivarassukackas@Aivarass-MacBook-Pro-2.local>
2025-11-30 15:39:02 +0200

    update

--- modules/shared-vpc-self-link/main.tf
+++ modules/shared-vpc-self-link/main.tf
@@ -12,5 +12,5 @@ data "google_compute_subnetwork" "subnetwork" {
 
 locals {
   cluster_range  = [for i in data.google_compute_subnetwork.subnetwork.secondary_ip_range : i.range_name if length(regexall(".*pods", i.range_name)) > 0][0]
-  services_range = [for i in data.google_compute_subnetwork.subnetwork.secondary_ip_range : i.range_name if length(regexall(".*services", i.range_name)) > 0][0]
+  services_range = [for i in data.google_compute_subnetwork.subnetwork.secondary_ip_range : i.range_name if length(regexall(".*svc", i.range_name)) > 0][0]
 }
